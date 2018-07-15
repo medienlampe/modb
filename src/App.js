0cbedb451834f8ad8f687ab7ef72d5d774b21316
Sven Bischoff <sven@medienlampe.de>
2018-07-15 11:10:01 +0200

    Moved assets to dedicated folder.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React, { Component, Fragment as F } from 'react';
 import { Menu } from './components/Menu';
 import { Content } from './components/Content';
 
-import logo from './logo.svg';
-import './foundation.css';
-import './App.css';
+import './assets/foundation.css';
+import './assets/App.css';
 
 class App extends Component {
   render() {
